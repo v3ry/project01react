25c657975947d3a1d7ab6b743c7d4945d13606de
v3ry <v3ryfr@gmail.com>
2022-05-26 13:20:56 +0200

    deplacement imgs et creation header

---
+++ src/components/MyHeader.js
@@ -0,0 +1,37 @@
+import '../style.css';
+
+function MyHeader() {
+  return (
+    <div className="MyHeader">
+      <header>
+        <img src="/imgs/bandeau.png" alt="Image cuisine baniere" className="mainImg"/>
+        <div className="glitch-bloc">
+            <h1 className="invisible-text mainTitle">Le Gras C'est La Vie</h1>
+            <h1 className="glitchedAnim mainTitle">Le Gras C'est La Vie</h1>
+            <h1 className="glitchedAnim mainTitle">Le Gras C'est La Vie</h1>
+            <h1 className="glitchedAnim mainTitle">Le Gras C'est La Vie</h1>
+          </div>
+        <div className="buttonBlock">
+            <div className="button">
+                <a href="./index.html"><img src="/imgs/baniere.jpg" alt="bouton accueil"/></a>
+                <h2 id="bAccueil">Accueil</h2>
+            </div>
+            <div className="button">
+                <a href="./sucree.html"><img src="/imgs/croissant.jpg" alt="bouton recette sucré"/></a>
+                <h2 id="bSucree">Sucré</h2>
+            </div>
+            <div className="button">
+                <a href="../salee.html"><img src="/imgs/pasta.jpg" alt="bouton recette salée"/></a>
+                <h2 id="bSalee">Salé</h2>
+            </div>
+            <div className="button">
+                <a href="../contact.html"><img src="/imgs/contact.jpg" alt="bouton contact"/></a>
+                <h2 id="bContact">Contact</h2>
+            </div>
+        </div>
+    </header>
+    </div>
+  );
+}
+
+export default MyHeader;
