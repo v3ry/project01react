a9ed8da11e38006ba204204daef3dd8e4a16aedf
v3ry <v3ryfr@gmail.com>
2022-05-26 13:24:53 +0200

    ajout footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyHeader from './components/MyHeader';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <MyHeader/>
+
+      <Footer/>
     </div>
   );
 }
